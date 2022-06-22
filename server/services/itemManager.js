811e983265f823bacefa039786b975ca2fe72e00
Shamgar1 <ayelet2708@gmail.com>
2022-06-22 15:24:34 +0300

    changeex4

--- server/services/itemManager.js
+++ server/services/itemManager.js
@@ -1,24 +1,13 @@
-const { Sequelize } = require("sequelize");
-const { request } = require("express");
-const database = require("mime-db");
-const { values } = require("sequelize/lib/operators");
-const { INSERT } = require("sequelize/lib/query-types");
-const PokemonClient = require("../clients/pokemonClient");
-
-const Items = require("../db/models/items");
-const { findAll } = require("sequelize/lib/model");
-// const Items = require("../db/models");
+const PokemonClient = require("../clients/pokemon_client");
+const PokemonClient = require("../clients/pokemon_client");
 
 class ItemManager {
 	constructor() {
 		this.pokemonClient = new PokemonClient();
-		this.items = []; //TODO: remove, items should be stored to DB using Item sequelize model
+		this.items = [];
 	}
 
-	getItems = async () => {
-		let items = await Items.findAll();
-		return items;
-	};
+	getItems = () => this.items;
 
 	handleItem = async (item) => {
 		if (this._isNumber(item)) {
@@ -28,25 +17,21 @@ class ItemManager {
 			return await this.fetchAndAddManyPokemon(item);
 		}
 
-		await this.addItem(item);
+		this.addItem(item);
 	};
 
-	addItem = async (itemName) => {
-		this.items.push(itemName);
-		Items.create({ itemName });
-		await this.getItems();
+	addItem = (item) => {
+		this.items.push(item);
 	};
 
-	addPokemonItem = async (pokemon) => {
-		await this.addItem(`Catch ${pokemon.name}`);
-		// this.getItems();
-		// this.getItems();
+	addPokemonItem = (pokemon) => {
+		this.addItem(`Catch ${pokemon.name}`);
 	};
 
 	fetchAndAddPokemon = async (pokemonId) => {
 		try {
 			const pokemon = await this.pokemonClient.getPokemon(pokemonId);
-			await this.addPokemonItem(pokemon);
+			this.addPokemonItem(pokemon);
 		} catch (error) {
 			this.addItem(`Pokemon with ID ${pokemonId} was not found`);
 		}
@@ -57,17 +42,15 @@ class ItemManager {
 			const pokemons = await this.pokemonClient.getManyPokemon(
 				inputValue.replace("/ /g", "").split(",")
 			);
-			pokemons.forEach(await this.addPokemonItem());
+			pokemons.forEach(this.addPokemonItem);
 		} catch (error) {
 			console.error(error);
 			this.addItem(`Failed to fetch pokemon with this input: ${inputValue}`);
 		}
 	};
 
-	deleteItem = async (item) => {
-		// this.items = this.items.filter((i) => i !== item);
-		const row = await Items.destroy({ where: { id: item.id } });
-		return await this.getItems();
+	deleteItem = (item) => {
+		this.items = this.items.filter((i) => i !== item);
 	};
 
 	_isNumber = (value) => !isNaN(Number(value));
